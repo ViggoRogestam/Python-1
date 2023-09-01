8223056c27d8ea4e59d0a4ba7d215a588e0ab887
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-09-01 11:30:08 +0200

    loopar övningar

---
+++ Pyhton 1/Lektion 03/while-loop.py
@@ -0,0 +1,23 @@
+# While-loopar
+"""
+counter = 0
+while counter <= 10:
+    print(counter)
+    counter +=1
+
+while True:
+    print("This program is running!")
+    goAgain = input("Go again y/n?: ")
+    if goAgain == "n":
+        break
+print("Program stopped running.")
+
+counter = 0
+while True:
+    print(counter)
+    goAgain = input("Go again y/n?: ")
+    counter += 1
+    if goAgain == "n":
+        break
+print("Program stopped running.")
+"""
