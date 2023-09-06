ddc5da561c5188b430b5d485ee5e58ed893d3405
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-09-06 09:43:22 +0200

    övning

---
+++ Pyhton 1/Lektion 05/list.demo.py
@@ -0,0 +1,5 @@
+deltagare = ["Lina", "Gunilla", "Erik", "Carl"]
+i = 0
+while i < len(deltagare):
+    print("Välkommer " + deltagare[i])
+    i += 1
---
+++ Pyhton 1/övningsuppgifter/6.2/6.1.py
@@ -0,0 +1,7 @@
+text = input("ange en text: ")
+letter = input("ange ")
+text = text.lower()
+
+i = 0
+while i <= len(text):
+    text.find()
