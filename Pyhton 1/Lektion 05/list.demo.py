c4c40d672e2af565dfb21ed958f497089ea64d15
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-09-06 10:24:52 +0200

    övningsuppgifter

--- Pyhton 1/Lektion 05/list.demo.py
+++ Pyhton 1/Lektion 05/list.demo.py
@@ -2,4 +2,10 @@ deltagare = ["Lina", "Gunilla", "Erik", "Carl"]
 i = 0
 while i < len(deltagare):
     print("Välkommer " + deltagare[i])
+    deltagare.remove(deltagare[i])
     i += 1
+
+deltagare = ["Lina", "Gunilla", "Erik", "Carl"]
+for namn in deltagare:
+    print(f"Väkommen {namn}")
+    del deltagare[namn]
---
+++ Pyhton 1/Lektion 05/övning.py
@@ -0,0 +1,15 @@
+# Skriv ut talen 0 - 9 utan att använda while
+"""lista = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9]"""
+my_list = list(range(10))
+for tal in my_list:
+    print(tal)
